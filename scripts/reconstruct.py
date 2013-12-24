25d5fc4ac9dc6b7fa5afaaba4e79a5c1ea789e7c
Cysu <st.cysu@gmail.com>
2013-12-24 10:58:31 +0800

    Experiment with reconstruction but failed

--- scripts/reconstruct.py
+++ scripts/reconstruct.py
@@ -5,10 +5,12 @@ import cPickle
 import numpy
 import theano
 import theano.tensor as T
+from scipy.spatial.distance import cdist
 
 from reid.preproc import imageproc
 from reid.datasets import Datasets
-from reid.models.neural_net import AutoEncoder
+from reid.models.layers import FullConnLayer, ConvPoolLayer
+from reid.models.neural_net import NeuralNet, AutoEncoder
 from reid.models import active_functions as actfuncs
 from reid.models import cost_functions as costfuncs
 from reid.optimization import sgd
@@ -19,14 +21,16 @@ from reid.utils.data_manager import DataLoader
 
 _cached_datasets = '../cache/reconstruct_datasets.pkl'
 _cached_model = '../cache/reconstruct_model.pkl'
+_cached_output = '../cache/reconstruct_output.pkl'
 _cached_distmat = '../cache/reconstruct_distmat.pkl'
 
 
 def _preproc(image):
-    image = imageproc.imresize(image, (160, 80, 3))
-    image = imageproc.subtract_luminance(image)
-    image = imageproc.scale_per_channel(image, [0, 1])
-    return image
+    image = imageproc.imresize(image, (80, 40, 3))
+    image = numpy.rollaxis(image, 2)
+    # image = imageproc.subtract_luminance(image)
+    # image = imageproc.scale_per_channel(image, [0, 1])
+    return image / 255.0
 
 def _prepare_data(load_from_cache=False, save_to_cache=False):
     if load_from_cache:
@@ -80,9 +84,9 @@ def _prepare_data(load_from_cache=False, save_to_cache=False):
 
         X = imageproc.images2mat(X).astype(theano.config.floatX)
         Y = imageproc.images2mat(Y).astype(theano.config.floatX)
-        
+
         datasets = Datasets(X, Y)
-        datasets.split(train_ratio=0.5, valid_ratio=0.3)
+        datasets.split(train_ratio=0.8, valid_ratio=0.1)
 
     if save_to_cache:
         with open(_cached_datasets, 'wb') as f:
@@ -99,12 +103,28 @@ def _train_model(datasets, load_from_cache=False, save_to_cache=False):
         # Build model
         print "Building model ..."
 
-        model = AutoEncoder(layer_sizes=[38400, 1024, 1024],
-                            active_funcs=[actfuncs.sigmoid, actfuncs.sigmoid],
-                            cost_func=costfuncs.mean_square_error,
-                            error_func=costfuncs.mean_square_error)
+        # model = AutoEncoder(layer_sizes=[9600, 2400, 2400, 2400, 2400],
+        #                     active_funcs=[actfuncs.sigmoid, actfuncs.sigmoid, actfuncs.sigmoid, actfuncs.sigmoid],
+        #                     cost_func=costfuncs.mean_square_error,
+        #                     error_func=costfuncs.mean_square_error)
 
-        sgd.train(model, datasets, n_epoch=100, learning_rate=1e-3)
+        layers = [ConvPoolLayer((128,3,5,5), (2,2), (3,80,40), actfuncs.sigmoid, False),
+                  ConvPoolLayer((64,128,5,5), (1,1), None, actfuncs.sigmoid, False),
+                  ConvPoolLayer((32,64,5,5), (1,1), None, actfuncs.sigmoid, True),
+                  FullConnLayer(9600, 9600, actfuncs.sigmoid),
+                  FullConnLayer(9600, 9600, actfuncs.sigmoid)]
+
+        model = NeuralNet(layers, costfuncs.mean_square_error, costfuncs.mean_square_error)
+
+        import copy
+        pretrain_datasets = copy.copy(datasets)
+        pretrain_datasets.train_y = pretrain_datasets.train_x
+        pretrain_datasets.valid_y = pretrain_datasets.valid_x
+        pretrain_datasets.test_y = pretrain_datasets.test_x
+
+        sgd.train(model, pretrain_datasets, n_epoch=10, learning_rate=1e-3, learning_rate_decr=1.0)
+
+        sgd.train(model, datasets, n_epoch=100, learning_rate=1e-3, learning_rate_decr=1.0)
 
     if save_to_cache:
         with open(_cached_model, 'wb') as f:
@@ -129,14 +149,12 @@ def _get_distance(model, data, load_from_cache=False, save_to_cache=False):
         X = imageproc.images2mat(X).astype(theano.config.floatX)
         Y = imageproc.images2mat(Y).astype(theano.config.floatX)
 
-        R = output_func(X)
+        R = [0] * X.shape[0]
+        for i in xrange(X.shape[0]):
+            R[i] = output_func(X[i:i+1, :]).ravel()
+        R = numpy.asarray(R)
 
-        nr, ny = R.shape[0], Y.shape[0]
-        D = numpy.zeros((nr, ny), dtype=theano.config.floatX)
-
-        for i in xrange(nr):
-            for j in xrange(ny):
-                D[i, j] = ((R[i]-Y[j])**2).sum()
+        D = cdist(R, Y, 'euclidean')
 
         G = numpy.asarray([pid for pid, image in data[0]])
         P = numpy.asarray([pid for pid, image in data[1]])
@@ -145,17 +163,20 @@ def _get_distance(model, data, load_from_cache=False, save_to_cache=False):
         with open(_cached_distmat, 'wb') as f:
             cPickle.dump((D, G, P), f, protocol=cPickle.HIGHEST_PROTOCOL)
 
+    with open(_cached_output, 'wb') as f:
+        cPickle.dump((X, Y, R), f, protocol=cPickle.HIGHEST_PROTOCOL)
+
     return (D, G, P)
 
 if __name__ == '__main__':
     views_data, datasets = _prepare_data(load_from_cache=True,
                                          save_to_cache=False)
 
-    model = _train_model(datasets, load_from_cache=True, save_to_cache=False)
+    model = _train_model(datasets, load_from_cache=False, save_to_cache=True)
 
     distmat, glabels, plabels = _get_distance(model, views_data,
-                                              load_from_cache=True,
-                                              save_to_cache=False)
+                                              load_from_cache=False,
+                                              save_to_cache=True)
 
     print cmc.count(distmat, glabels, plabels, 100)
 
