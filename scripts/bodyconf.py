bfaf1e5937edac74ef20230d313b91f7d46c2add
Cysu <st.cysu@gmail.com>
2014-02-12 13:05:00 +0800

    Decompose pedestrian into body parts groups

---
+++ scripts/bodyconf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+pixval = {
+    'hair': 10,
+    'head': 20,
+    'upper': 30,
+    'arms': 40,
+    'lower': 50,
+    'legs': 60,
+    'feet': 70
+}
+
+groups = [
+    [10, 20],
+    [30, 40],
+    [50, 60],
+    [70]
+]
--- scripts/learnattr.py
+++ scripts/learnattr.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import os
+import numpy
 from cache_manager import CacheManager
 
 
 cachem = CacheManager(os.path.join('..', 'cache'), 'attr')
 
 
-@cachem.save('rawdata')
+@cachem.save('raw')
 def load_data(datasets):
     """Load images, corresponding body parts maps and attributes vectors
 
@@ -19,7 +20,7 @@ def load_data(datasets):
     ``(image, body_parts_map, attribute)``.
     """
 
-    data = cachem.load('rawdata')
+    data = cachem.load('raw')
 
     if data is None:
         from scipy.io import loadmat
@@ -41,7 +42,50 @@ def load_data(datasets):
     return data
 
 
+@cachem.save('decomp')
+def decomp_body(rawdata, groups, dilation_radius=3):
+    """Decompose pedestrain into several body parts groups
+
+    This function will generate an image containing only the region of
+    particular body parts for each group.
+
+    Args:
+        rawdata: A list of pedestrian tuples returned by ``load_data``
+        groups: A list of groups' pixel values
+        dilation_radius: The radius of dilation to be performed on group region
+
+    Returns a list of tuples. Each is for one pedestrian in the form of
+    ``([img_0, img_1, ... , img_m], attribute)`` where ``m`` is the number
+    of body parts groups.
+    """
+
+    data = cachem.load('decomp')
+
+    if data is None:
+        import skimage.morphology as morph
+        selem = morph.square(2*dilation_radius)
+
+        data = [0] * len(rawdata)
+
+        for i, (img, bpmap, attr) in enumerate(rawdata):
+            imgs = [0] * len(groups)
+
+            for j, grp in enumerate(groups):
+                # mask = dilate(region_0 | region_1 | ... | region_k)
+                regions = [(bpmap == pixval) for pixval in grp]
+                mask = reduce(lambda x, y: x|y, regions)
+                mask = morph.binary_dilation(mask, selem)
+
+                imgs[j] = img * numpy.expand_dims(mask, axis=2)
+
+            data[i] = (imgs, attr)
+
+    return data
+
+
 if __name__ == '__main__':
     import attrconf
+    import bodyconf
 
-    rawdata = load_data(attrconf.datasets)
+    data = load_data(attrconf.datasets)
+    data = decomp_body(data, bodyconf.groups)
