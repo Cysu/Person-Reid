6f66f4dc157be0b84847e8db1503c8c28da437fe
Cysu <st.cysu@gmail.com>
2014-02-12 14:13:28 +0800

    Move Dataset class to utils and refactor it

--- reid/utils/dataset.py
+++ reid/utils/dataset.py
@@ -5,9 +5,28 @@ import numpy
 import theano
 
 
-class Datasets(object):
+class Dataset(object):
+    """Dataset for model training, validation and testing
 
-    def __init__(self, X=None, Y=None, train_set=None, valid_set=None, test_set=None):
+    The object contains data stored as theano shared variables. Both feature and
+    label of a sample are numpy vectors.
+    """
+
+    def __init__(self, X=None, Y=None,
+                 train_set=None, valid_set=None, test_set=None):
+        """Initialize the Dataset
+
+        Args:
+            X, Y: Numpy matrix with each row representing a sample
+            train_set, valid_set, test_set: Each is a numpy matrix tuple (X, Y)
+                representing features and labels
+
+            If ``X`` and ``Y`` are specified, the Dataset will use them as all
+            the samples. Otherwise, if ``train_set``, ``valid_set`` and
+            ``test_set`` are specified, the Dataset will stack them together to
+            as all the samples.
+        """
+
         if X is not None and Y is not None:
             self.X = X
             self.Y = Y
@@ -25,6 +44,14 @@ class Datasets(object):
             raise ValueError("Invalid argument combination")
 
     def split(self, train_ratio, valid_ratio):
+        """Split all the samples into training, validation and testing sets
+        randomly
+
+        Args:
+            train_ratio: The ratio between training and all the samples
+            valid_ratio: The ratio between validation and all the samples
+        """
+
         m = self.X.shape[0]
 
         m_train = int(m * train_ratio)
@@ -33,8 +60,8 @@ class Datasets(object):
         p = numpy.random.permutation(m)
 
         train_ind = p[0 : m_train]
-        valid_ind = p[m_train : (m_train+m_valid)]
-        test_ind = p[(m_train+m_valid) : ]
+        valid_ind = p[m_train : m_train+m_valid]
+        test_ind = p[m_train+m_valid : ]
 
         self.train_x = self._create_shared(self.X[train_ind, :])
         self.train_y = self._create_shared(self.Y[train_ind, :])
@@ -43,17 +70,11 @@ class Datasets(object):
         self.test_x = self._create_shared(self.X[test_ind, :])
         self.test_y = self._create_shared(self.Y[test_ind, :])
 
-    def add_train_data(self, x, y):
-        self.train_x = numpy.vstack((self.train_x.get_value(borrow=True), x));
-        self.train_y = numpy.vstack((self.train_y.get_value(borrow=True), y));
-        numpy.random.shuffle(self.train_x)
-        numpy.random.shuffle(self.train_y)
-        self.train_x = self._create_shared(self.train_x);
-        self.train_y = self._create_shared(self.train_y);
-
     def get_train_size(self):
         return self.train_x.get_value(borrow=True).shape[0]
 
+    @staticmethod
     def _create_shared(self, x):
-        return theano.shared(numpy.asarray(x,
-            dtype=theano.config.floatX), borrow=True)
+        return theano.shared(numpy.asarray(x, dtype=theano.config.floatX),
+                             borrow=True)
+
